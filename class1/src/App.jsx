b114ddc7275391dc4416f79c7000196ed6bc421f
Vidyasagarkt <vidyasagarkt408423@gmail.com>
2023-03-01 22:46:39 +0530

    push class1

---
+++ class1/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sample = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default Sample
---
+++ class1/src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import {createRoot} from 'react-dom/client'
+import Sample from './App';
+
+
+createRoot(document.getElementById('root')).render(<Sample/>)
